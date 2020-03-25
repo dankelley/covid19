0ec86e9240ea703e729c10fade2b176f1fa0d9c0
dankelley <kelley.dan@gmail.com>
2020-03-25 14:09:00 -0300

    add daily-increase panel; list daily levels

--- covid19.R
+++ covid19.R
@@ -53,18 +53,18 @@ for (region in regions) {
     ## recovered <- acquireCovid19(paste0(base, "/time_series_19-covid-Recovered.csv"), region=region)
 
     if (!interactive()) png(paste0("covid19_", region, ".png"),
-                            width=5, height=5, unit="in", res=120, pointsize=10)
+                            width=5, height=4.5, unit="in", res=120, pointsize=13)
 
-    par(mfrow=c(2,1), pch=20)
+    par(mfrow=c(3,1), pch=20)
 
-    oce::oce.plot.ts(confirmed$time, confirmed$data, type="p", drawTimeRange=FALSE, col="gray",
+    oce::oce.plot.ts(confirmed$time, confirmed$data, type="o", drawTimeRange=FALSE, col="gray",
                      xlab="Time", ylab="Case Count", mar=c(2,3,1,1.5))
     points(confirmed$time[recent], confirmed$data[recent], pch=20, col="black")
-    mtext(region, adj=0, cex=0.9)
+    mtext(region, adj=0, cex=par("cex"))
     now <- lubridate::with_tz(Sys.time(), "UTC")
     ##mtext(paste("Graph updated", format(now, "%Y %b %d (%H:%M %Z)")), adj=1, cex=0.9)
-    mtext(paste(format(now, "%Y %b %d")), adj=1, cex=0.9)
-    points(deaths$time, trimZeros(deaths$data), col="red", type="b")
+    mtext(paste(format(now, "%Y %b %d")), adj=1, cex=par("cex"))
+    points(deaths$time, trimZeros(deaths$data), col="red", type="o")
     ## points(recovered$time, trimZeros(recovered$data), col="green3")
     ## legend("topleft", pt.cex=1.4, cex=0.9, pch=20, bg="white",
     ##        col=c("gray", "black", "green3", "red"),
@@ -80,8 +80,7 @@ for (region in regions) {
     y <- log10(confirmed$data)
     y[!is.finite(y)] <- NA
     ylim <- c(0, max(y, na.rm=TRUE))
-    oce::oce.plot.ts(confirmed$time, y, ylim=ylim,
-                     type="p", axes=FALSE, col="gray",
+    oce::oce.plot.ts(confirmed$time, y, ylim=ylim, type="o", axes=FALSE, col="gray",
                      xlab="Time", ylab="Case Count", mar=c(2, 3, 1, 1.5))
     oce::oce.axis.POSIXct(side=1, drawTimeRange=FALSE)
     box()
@@ -95,11 +94,11 @@ for (region in regions) {
         smallTics <- c(smallTics,      power + log10(2:9))
         rug(side=4, x=power, tcl=tcl, lwd=par("lwd"))
         if (power < 4L) {
-            mtext(10^power, side=2, at=power, line=0.5)
+            mtext(10^power, side=2, at=power, line=0.5, cex=par("cex"))
         } else {
-            mtext(substitute(10^A, list(A=power)), side=2, at=power, line=0.5)
+            mtext(substitute(10^A, list(A=power)), side=2, at=power, line=0.5, cex=par("cex"))
         }
-        abline(h=power, lty="dotted", col="gray")
+        abline(h=power, lty="dotted", col="lightgray")
     }
     smallTics <- unique(smallTics[par("usr")[3] < smallTics & smallTics < par("usr")[4]])
     rug(side=2, x=smallTics, tcl=0.5*tcl, lwd=par("lwd"))
@@ -119,12 +118,17 @@ for (region in regions) {
         doubleTime <- log10(2) / growthRate
         mtext(sprintf("Doubling time: %.1fd", doubleTime), side=3, adj=1, cex=par("cex"))
     }
-    points(deaths$time, log10(deaths$data), col="red", pch=20)
+    points(deaths$time, log10(deaths$data), col="red", pch=20, type="o")
     ## points(recovered$time, log10(recovered$data), col="green3")
     ## Skip legend in bottom panel
     ##> legend("topleft", pt.cex=1.4, cex=0.9, pch=20, bg="white",
     ##>        col=c("gray", "black", "green3", "red"),
     ##>        legend=c("Confirmed", "Confirmed", "Recoveries", "Deaths"))
+
+    oce::oce.plot.ts(confirmed$time[-1], diff(confirmed$data), type="o", drawTimeRange=FALSE, col="gray",
+                     xlab="Time", ylab="Daily Increase", mar=c(2,3,1,1.5))
+    points(confirmed$time[recent][-1], diff(confirmed$data[recent]), pch=20)
+    points(deaths$time[-1], diff(deaths$data), col="red", pch=20, type="o")
 
     if (!interactive()) dev.off()
 }

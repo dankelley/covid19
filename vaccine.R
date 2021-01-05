d37a8f35dfa6ece3b5ffd24d49a9654e0cc04fee
dankelley <kelley.dan@gmail.com>
2021-01-05 17:06:49 -0400

    uniform scales; show time to 100%

--- vaccine.R
+++ vaccine.R
@@ -1,5 +1,7 @@
 library(oce)
 
+uniformScale <- TRUE
+
 file <- "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 d <- read.csv(file, header=TRUE)
 
@@ -11,7 +13,12 @@ vaccineStart <- as.POSIXct("2020-12-10")
 now <- Sys.Date()
 d$time <- as.POSIXct(d$date)
 print(c(vaccineStart, now))
-d <- d[d$time > vaccineStart, ] # ignore old stuff
+d <- d[d$time >= vaccineStart, ] # ignore old stuff
+
+locations <- c("Canada", "United Kingdom", "United States")
+prange <- function(x) range(x[is.finite(x) & x > 0], na.rm=TRUE)
+ylim <- if (uniformScale)
+    range(sapply(locations, function(l) prange(d[d$location==l,]$total_vaccinations_per_hundred))) else NULL
 
 width <- 8
 height <- 5.5
@@ -29,10 +36,15 @@ for (location in c("Canada", "United Kingdom", "United States")) {
         day <- (dd$time - dd$time[1]) / 86400
         v100 <- dd$total_vaccinations_per_hundred
         m <- lm(v100 ~ day)
-        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
+        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, ylim=ylim,
+                    xlab="", ylab="Vaccines/100", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
         mtext(location, adj=1, cex=1.1*par("cex"))
-        mtext(sprintf(" Trend: %.3f vaccinations/100 persons/day", coef(m)[2]), adj=0, line=-1, cex=0.9*par("cex"))
-        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", log="y", logStyle="decade", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
+        trend <- coef(m)[2]
+        mtext(sprintf(" Trend: %.3f vaccinations/100 persons/day", trend), adj=0, line=-1, cex=0.9*par("cex"))
+        yearsToAll <- 100 / trend / 365
+        mtext(sprintf(" Implies %.1f years to 100%%", yearsToAll), adj=0, line=-2, cex=0.9*par("cex"))
+        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, ylim=ylim,
+                    xlab="", ylab="Vaccines/100", log="y", logStyle="decade", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
         # mtext(sprintf("General Life Expectancy: %.1fy", dd$life_expectancy[1]), cex=par("cex"))
         cat("#", location, "\n")
         cat(oce::vectorShow(dd$population_density[1]))

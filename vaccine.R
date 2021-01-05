73ba66101cda1f7ffe9ac033b267a6242dd98800
dankelley <kelley.dan@gmail.com>
2021-01-05 16:42:23 -0400

    update vaccine.R

--- vaccine.R
+++ vaccine.R
@@ -1,60 +1,53 @@
 library(oce)
 
 file <- "https://covid.ourworldindata.org/data/owid-covid-data.csv"
-if (!exists("d")) d <- read.csv(file, header=TRUE) else cat("using cached data\n")
+d <- read.csv(file, header=TRUE)
 
 variables <- sort(names(d))
+cat("variables follow (in alphabetical order)\n")
+print(variables)
+
 vaccineStart <- as.POSIXct("2020-12-10")
 now <- Sys.Date()
 d$time <- as.POSIXct(d$date)
 print(c(vaccineStart, now))
+d <- d[d$time > vaccineStart, ] # ignore old stuff
 
 width <- 8
 height <- 5.5
 res <- 200
 pointsize <- 11
 if (!interactive())
-    png("vaccine.png",
-        width=width,
-        height=height,
-        unit="in",
-        res=res,
-        pointsize=pointsize)
-
+    png("vaccine.png", width=width, height=height, unit="in", res=res, pointsize=pointsize)
 par(mfcol=c(2,3))
 for (location in c("Canada", "United Kingdom", "United States")) {
-    placeOK <- d$location == location
-    timeOK <- d$time > vaccineStart
-    dataOK <- is.finite(d$total_vaccinations_per_hundred) & d$total_vaccinations_per_hundred > 0
-    dlook <- d[placeOK & timeOK & dataOK, ]
-    if (nrow(dlook)) {
-        day <- (dlook$time - dlook$time[1]) / 86400
-        v100 <- dlook$total_vaccinations_per_hundred
+    dd <- d[d$location == location,]
+    ## Trim the zero values (for old oce 1.2.0 and earlier, which does not ignore the <=0 cases)
+    dataOK <- is.finite(dd$total_vaccinations_per_hundred) & dd$total_vaccinations_per_hundred > 0
+    dd <- dd[dataOK, ]
+    if (nrow(dd)) {
+        day <- (dd$time - dd$time[1]) / 86400
+        v100 <- dd$total_vaccinations_per_hundred
         m <- lm(v100 ~ day)
-        oce.plot.ts(dlook$time, dlook$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
+        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
         mtext(location, adj=1, cex=1.1*par("cex"))
         mtext(sprintf(" Trend: %.3f vaccinations/100 persons/day", coef(m)[2]), adj=0, line=-1, cex=0.9*par("cex"))
-        oce.plot.ts(dlook$time, dlook$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", log="y", logStyle="decade", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
-        # mtext(sprintf("General Life Expectancy: %.1fy", dlook$life_expectancy[1]), cex=par("cex"))
-
+        oce.plot.ts(dd$time, dd$total_vaccinations_per_hundred, xlab="", ylab="Vaccines/100", log="y", logStyle="decade", drawTimeRange=FALSE, grid=TRUE, xlim=c(vaccineStart, now), type="o")
+        # mtext(sprintf("General Life Expectancy: %.1fy", dd$life_expectancy[1]), cex=par("cex"))
         cat("#", location, "\n")
-        cat(oce::vectorShow(dlook$population_density[1]))
-        cat(oce::vectorShow(dlook$median_age[1]))
-        cat(oce::vectorShow(dlook$aged_65_older[1]))
-        cat(oce::vectorShow(dlook$aged_70_older[1]))
-        cat(oce::vectorShow(dlook$gdp_per_capita[1]))
-        cat(oce::vectorShow(dlook$extreme_poverty[1]))
-        cat(oce::vectorShow(dlook$cardiovasc_death_rate[1]))
-        cat(oce::vectorShow(dlook$diabetes_prevalence[1]))
-        cat(oce::vectorShow(dlook$female_smokers[1]))
-        cat(oce::vectorShow(dlook$male_smokers[1]))
-        cat(oce::vectorShow(dlook$hospital_beds_per_thousand[1]))
-        cat(oce::vectorShow(dlook$life_expectancy[1]))
-        cat(oce::vectorShow(dlook$human_development_index[1]))
-
-
-
-
+        cat(oce::vectorShow(dd$population_density[1]))
+        cat(oce::vectorShow(dd$median_age[1]))
+        cat(oce::vectorShow(dd$aged_65_older[1]))
+        cat(oce::vectorShow(dd$aged_70_older[1]))
+        cat(oce::vectorShow(dd$gdp_per_capita[1]))
+        cat(oce::vectorShow(dd$extreme_poverty[1]))
+        cat(oce::vectorShow(dd$cardiovasc_death_rate[1]))
+        cat(oce::vectorShow(dd$diabetes_prevalence[1]))
+        cat(oce::vectorShow(dd$female_smokers[1]))
+        cat(oce::vectorShow(dd$male_smokers[1]))
+        cat(oce::vectorShow(dd$hospital_beds_per_thousand[1]))
+        cat(oce::vectorShow(dd$life_expectancy[1]))
+        cat(oce::vectorShow(dd$human_development_index[1]))
     } else {
         warning("'", location, "' not found\n")
     }

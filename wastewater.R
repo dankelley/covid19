5e2836fb39f15b6c332b7597aab996ee9cee001b
dankelley <kelley.dan@gmail.com>
2022-05-12 15:40:55 -0300

    improve spline

--- wastewater.R
+++ wastewater.R
@@ -1,3 +1,4 @@
+tsmooth <- 14 # days used in computing spline df
 library(oce)
 url <- "https://health-infobase.canada.ca/src/data/covidLive/wastewater/covid19-wastewater.csv"
 file <- gsub(".*/", "", url)
@@ -12,11 +13,12 @@ if (file.exists(file)) {
 d <- read.csv(file)
 dr <- subset(d, region=="Halifax")
 t <- as.POSIXct(dr$Date, tz="UTC")
+dt <- as.numeric(difftime(max(t), min(t), units="days"))
 viralLoad <- dr$viral_load
 png("wastewater_halifax.png", unit="in", width=7, height=3, res=100)
 oce.plot.ts(t, viralLoad, type="p", pch=20, col="gray",
     grid=TRUE, ylab="Viral Load")
-sp <- smooth.spline(t, viralLoad, df=length(t)/30)
+sp <- smooth.spline(t, viralLoad, df=dt/tsmooth)
 smoothed <- predict(sp)
 lines(smoothed$x, smoothed$y, col=2, lwd=2)
 dev.off()

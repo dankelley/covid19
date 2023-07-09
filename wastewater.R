ba094395f85d497fef9e561f26a1789a3017754d
dankelley <kelley.dan@gmail.com>
2023-07-09 19:11:10 -0300

    wastewater: key on new column

--- wastewater.R
+++ wastewater.R
@@ -1,40 +1,41 @@
+url <- "https://health-infobase.canada.ca/src/data/covidLive/wastewater/covid19-wastewater.csv"
 library(oce)
-tsmooth <- 14 # days used in computing spline df
-downloadCache <- function(url, days=1)
+tsmooth <- 3 # days used in computing spline df
+downloadCache <- function(url, days=7)
 {
     file <- gsub(".*/", "", url)
     if (file.exists(file)) {
-        mtime <- file.mtime(file)
-        if (mtime < Sys.time() - days*86400)
+        if (file.mtime(file) < Sys.time() - days*86400)
             download.file(url, file)
-        else cat("using cached file", file, "\n")
+        else
+            cat("using cached file", file, "\n")
     } else {
         download.file(url, file)
     }
     file
 }
-file <- downloadCache("https://health-infobase.canada.ca/src/data/covidLive/wastewater/covid19-wastewater.csv", 1)
+file <- downloadCache(url, 1)
 
 d <- read.csv(file)
 d$time <- as.POSIXct(d$Date, tz="UTC")
-dt <- as.numeric(difftime(max(d$time), min(d$time), units="days"))
-df <- dt / tsmooth
 
-regions <- unique(d$region)
+locations <- unique(d$Location)
 tlim <- range(d$time)
-for (r in regions) {
-    cat("plotting wastewater for", r, "\n")
-    dr <- subset(d, region==r)
-    t <- dr$time
-    y <- dr$viral_load
-    png(paste0("wastewater_", r, ".png"),
+for (l in sort(locations)) {
+    dd <- subset(d, Location==l)
+    t <- as.POSIXct(dd$Date)
+    y <- dd$viral_load
+    png(paste0("wastewater_", l, ".png"),
         unit="in", width=7, height=2.5, res=100)
-    oce.plot.ts(t, y, type="p", pch=20, col="gray",
+    oce.plot.ts(t, y, type="l", col="gray",
         drawTimeRange=FALSE, xlim=tlim, grid=TRUE, ylab="Viral Load")
+    nx <- length(unique(t))
+    df <- max(nx/tsmooth, 10)
+    cat("plotting ", l, " (df=", df, ", nx=", nx, ")\n", sep="")
     sp <- smooth.spline(t, y, df=df)
     smoothed <- predict(sp)
     lines(smoothed$x, smoothed$y, col=2, lwd=2)
-    mtext(r, adj=1)
+    mtext(l, adj=1)
     dev.off()
 }
 
